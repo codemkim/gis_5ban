cba3b88aa35017aaab06709178db1ed866813eb1
codemkim <inkin61@naver.com>
2021-08-30 10:14:03 +0900

    08/30 5ban class commit 5

--- articleapp/models.py
+++ articleapp/models.py
@@ -14,7 +14,7 @@ class Article(models.Model):
     project = models.ForeignKey(Project,
                                 on_delete=models.SET_NULL,
                                 related_name='article',
-                                null=True)
+                                null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
 
